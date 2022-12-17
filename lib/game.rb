b1065784966228c71fe5558b29c622307f225690
Bart Zweigoron <bzweigoron@gmail.com>
2022-12-16 21:03:11 -0500

    added guess input and checking

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,34 @@
 require './lib/hangman'
 
 game = Hangman.new()
-p game.word_array
+
 game.display_intro
+puts ''
+puts ''
+#p game.word_array
+
+
+until game.game_over
+  game.display_game_board
+  loop do
+    puts "Enter your guess"
+    guess = gets.chomp.upcase
+    if guess.match?(/[A-Z]/) && guess.length == 1
+      if game.check_for_valid_guess(guess)
+        break
+      end
+    
+    else 
+      puts ''
+      game.display_guessed_letters
+      puts "Invalid entry, try again.\n"
+    end
+  end
+  puts ''
+  game.display_guessed_letters
+
+end
+  
+
+
 
--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,19 +1,28 @@
 class Hangman
-  attr_reader :word_array
-  attr_accessor :current_round
+  attr_reader :word_array, :max_attempts
+  attr_accessor :current_round, :game_array, :guessed_letters, :attempts, :game_over
 
   def initialize(
     min_length = 4,
     max_length = 12,
-    game_rounds = 6,
-    current_round = 1
+    max_attempts = 6,
+    current_round = 1,
+    attempts = 0
   )
-  @min_length = min_length
-  @max_length = max_length
-  @game_rounds = game_rounds
-  @word_source = './res/wordlist.txt'
-  @word_array = determine_word
-  @current_round = current_round
+    @min_length = min_length
+    @max_length = max_length
+    @word_source = './res/wordlist.txt'
+    @word_array = determine_word
+    @game_array = Array.new(@word_array.length,' _ ')
+    @guessed_letters = Array.new(26,' - ')
+    @current_round = current_round
+    @attempts = attempts
+    @max_attempts = max_attempts
+    @game_over = false
+  end
+
+  def load_saved_game
+    puts "load game here"
   end
 
   def determine_word
@@ -27,9 +36,42 @@ class Hangman
 
   def display_intro
     puts 'Welcome to Hangman!'
+    puts "Type 'load' to continue your last game. Enter any other key to start a new game."
+    load_saved_game if response = gets.chomp
+    puts ''
     puts "The word was randomly selected from one of #{@word_list_size} options between #{@min_length} and #{@max_length} in length."
-    puts "You have #{@game_rounds} rounds to solve the puzzle "
+    puts "You have #{@max_attempts} attempt(s) to solve the puzzle "
   end
+
+  def display_game_board
+    puts "[ #{@game_array.join(' _ ')} ]"
+    puts ''
+  end
+
+  def display_guessed_letters
+    puts "| #{@guessed_letters.join('')} |"
+  end
+
+  def check_for_valid_guess(guess)
+    if @guessed_letters.include?(guess) 
+       false 
+    else
+      @guessed_letters[guess.ord - 65] = " #{guess} "
+       true
+    end
+  end
+
+  private
+
+  def determine_word
+    word_list = []
+    File.foreach(@word_source) do |line|
+      word_list.push(line.chomp) if line.chomp.length.between?(@min_length, @max_length)
+    end
+    @word_list_size = word_list.length.to_s
+    word_list.shuffle.first.split('')
+  end
+
 end
 
 
